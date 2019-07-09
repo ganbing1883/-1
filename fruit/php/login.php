a319fa082d199e7b5a2110de2e274721276e3775
ganbing1883 <2155391883@qq.com>
2019-07-09 23:27:20 +0800

    第三次提交

---
+++ fruit/php/login.php
@@ -0,0 +1,10 @@
+<?php
+$user= @$_REQUEST["user"];
+$pass= @$_REQUEST["pass"];
+if($user=="admin" || $pass=="123"){
+    echo "登录成功!";
+}else{
+    echo "用户名或密码不正确!";
+};
+// echo '{"msg":"这是就收到的数据","user":"'.$user.'","pass":"'.$pass.'"}';
+?>
